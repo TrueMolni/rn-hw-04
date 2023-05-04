046a341cc2775583143bb4a9d38ba0d3f03faf51
TrueMolni <truemolni@gmail.com>
2023-05-04 19:21:45 +0300

    5.0.1
    Added Camera and map to the app

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,42 +1,83 @@
 import React from "react";
-import { Button } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { Ionicons } from "@expo/vector-icons";
 
 import PostsScreen from "./PostsScreen";
 import CreatePostsScreen from "./CreatePostsScreen";
 import ProfileScreen from "./ProfileScreen";
 
+import SvgLogout from "../assets/svg/logoutIcon";
+import SvgPosts from "../assets/svg/postsIcon";
+import SvgCreate from "../assets/svg/createIcon";
+import SvgProfile from "../assets/svg/profileIcon";
+
 const Tabs = createBottomTabNavigator();
 
-const HomeScreen = ({ navigation }) => {
+const HomeTabs = ({ navigation }) => {
   return (
     <Tabs.Navigator>
       <Tabs.Screen
         options={{
-          headerStyle: {
-            backgroundColor: "#f4511e",
-          },
-          headerTintColor: "#fff",
+          title: "Публікації",
+          headerTintColor: "#000",
           headerTitleStyle: {
             fontWeight: "bold",
             fontSize: 20,
           },
           headerRight: () => (
-            <Button
+            <TouchableOpacity
+              style={{ marginRight: 10 }}
               onPress={() => navigation.navigate("Login")}
-              title="LOG OUT"
-              color="#fff"
-            />
+            >
+              <Image
+                source={require("../assets/logOut.jpg")}
+                style={{ width: 24, height: 24 }}
+              />
+            </TouchableOpacity>
           ),
+
+          tabBarIcon: ({ color, size }) => (
+            <SvgPosts color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
         }}
         name="Posts"
         component={PostsScreen}
       />
-      <Tabs.Screen name="Create" component={CreatePostsScreen} />
-      <Tabs.Screen name="Profile" component={ProfileScreen} />
+      <Tabs.Screen
+        options={{
+          title: "Створити публікацію",
+          tabBarIcon: ({ color, size }) => (
+            <SvgCreate color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+        name="Create"
+        component={CreatePostsScreen}
+      />
+      <Tabs.Screen
+        options={{
+          title: "Профіль",
+          tabBarIcon: ({ color, size }) => (
+            <SvgProfile color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
     </Tabs.Navigator>
   );
 };
 
-export default HomeScreen;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+export default HomeTabs;
--- Screens/PostsScreen.js
+++ Screens/PostsScreen.js
@@ -1,10 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-const PostsScreen = () => {
+import SvgMessage from "../assets/svg/messageIcon";
+import SvgMap from "../assets/svg/mapIcon";
+
+const POSTS = [];
+
+export default PostsScreen = ({ navigation, route }) => {
+  const [posts, setPosts] = useState(POSTS);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+
   return (
     <View style={styles.container}>
-      <Text>Posts Screen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.postsContainer}>
+            <Image style={styles.photo} source={{ uri: item.photo }} />
+            <Text style={styles.photoName}>{item.photoName}</Text>
+            <View style={styles.details}>
+              <TouchableOpacity
+                style={{ ...styles.detailsBlock, alignItems: "center" }}
+                // onPress={navigation.navigate("Comments")}
+              >
+                <SvgMessage />
+                <Text styles={{ marginLeft: 5 }}>0</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.detailsBlock}
+                onPress={() => {
+                  const location = item.location;
+                  const photoName = item.photoName;
+                  navigation.navigate("Map", { location, photoName });
+                }}
+              >
+                <SvgMap />
+                <Text style={styles.location}>{item.photoLocation}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -12,9 +62,34 @@ const PostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+
+  postsContainer: {
+    marginHorizontal: 16,
+    marginBottom: 24,
+  },
+  photo: {
+    width: "100%",
+    height: 240,
+    borderRadius: 10,
+  },
+  photoName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 8,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  detailsBlock: {
+    flexDirection: "row",
+  },
+  location: {
+    fontSize: 16,
+    marginLeft: 5,
+    textDecorationLine: "underline",
   },
 });
-
-export default PostsScreen;
