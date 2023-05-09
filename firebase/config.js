0ff5c162037fffd5deeb73c5fd197bfe5bcdc748
TrueMolni <truemolni@gmail.com>
2023-05-09 22:52:47 +0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -19,5 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const storage = getStorage();
+export const storage = getStorage(app);
 export const db = getFirestore(app);
